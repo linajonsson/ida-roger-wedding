ac499e8b64e047876aeb767c5b4c3d0cba8f225f
Lina Jonsson <lina.jonsson@digitalent.se>
2020-01-22 11:19:36 +0100

    Text image slider

---
+++ src/components/slideShow.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import nextArrow from "../images/icons/next.svg"
+import backArrow from "../images/icons/back.svg"
+
+const SlideShow = () => {
+  const [index, setIndex] = useState(0)
+  const { allFile } = useStaticQuery(
+    graphql`
+      query {
+        allFile(
+          sort: { fields: name, order: DESC }
+          filter: { relativeDirectory: { eq: "destination" } }
+        ) {
+          edges {
+            node {
+              id
+              name
+              childImageSharp {
+                fluid(maxWidth: 600) {
+                  ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  console.log(allFile)
+
+  //Minus 1 for array offset from 0
+  const length = allFile.edges.length - 1
+  const handleNext = () =>
+    index === length ? setIndex(0) : setIndex(index + 1)
+  const handlePrevious = () =>
+    index === 0 ? setIndex(length) : setIndex(index - 1)
+  const { node } = allFile.edges[index]
+  return (
+    <div className="slideshow">
+      <div>
+        <Img
+          fluid={node.childImageSharp.fluid}
+          key={node.id}
+          alt={node.name.replace(/-/g, " ").substring(2)}
+        />
+      </div>
+      <div>
+        <div className="slideshow-btn left" onClick={() => handlePrevious()}>
+          <img src={backArrow}></img>
+        </div>
+        <div className="slideshow-btn right" onClick={() => handleNext()}>
+          <img src={nextArrow}></img>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SlideShow
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,34 +2,31 @@ import React from "react"
 import Accomodation from "../components/content/accomodation"
 import BlockOne from "../components/content/blockOne"
 import Destination from "../components/content/destination"
+import DressCode from "../components/content/dressCode"
 import Happenings from "../components/content/happenings"
 import Menu from "../components/content/menu"
 import RsvpForm from "../components/content/rsvpForm"
+import Surroundings from "../components/content/surroundings"
 import ToastMadames from "../components/content/toastMadames"
 import Transportation from "../components/content/transportation"
 import WelcomeText from "../components/content/welcomeText"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import DressCode from "../components/content/dressCode"
-import Surroundings from "../components/content/surroundings"
-import Header from "../components/content/header"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="hej" />
     <Menu />
-    <div>
-      <BlockOne />
-      <WelcomeText />
-      <Destination />
-      <Accomodation />
-      <Transportation />
-      <Happenings />
-      <RsvpForm />
-      <DressCode />
-      <ToastMadames />
-      <Surroundings />
-    </div>
+    <BlockOne />
+    <WelcomeText />
+    <Destination />
+    <Accomodation />
+    <Transportation />
+    <Happenings />
+    <RsvpForm />
+    <DressCode />
+    <ToastMadames />
+    <Surroundings />
   </Layout>
 )
 

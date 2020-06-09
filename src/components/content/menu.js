19aaac399f1bd171c6860cd5850b1e62771a06f7
Lina Jonsson <lina.jonsson@digitalent.se>
2020-06-09 08:18:32 +0200

    Removed rsvp from menu

--- src/components/content/menu.js
+++ src/components/content/menu.js
@@ -27,7 +27,6 @@ export const menuTabs = [
   { id: "accomodation", text: "Boende" },
   { id: "transportation", text: "Hur tar man sig dit?" },
   { id: "happenings", text: "Happenings" },
-  { id: "rsvp", text: "OSA" },
   { id: "dresscode", text: "Klädkod" },
   { id: "toastmadames", text: "Toastmadames" },
   { id: "surroundings", text: "Omgivningen" },
